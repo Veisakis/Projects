50d52830eaaeebcdb81f6d3d2e83488b1ef52511
Veisakis <manosveisakis@gmail.com>
2022-01-17 01:33:34 +0200

    requirements and atom

--- names.py
+++ names.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calendar.db'
 
 db = SQLAlchemy(app)
 
+
 class Calendar(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
@@ -15,17 +16,19 @@ class Calendar(db.Model):
     def __repr__(self):
         return f"Calendar('{self.date}', '{self.name}')"
 
+
 @app.route('/')
 def home():
     if 'dt' in request.args:
         year = int(request.args['dt'].split("-")[0])
         month = int(request.args['dt'].split("-")[1])
         day = int(request.args['dt'].split("-")[2])
-        return Calendar.query.filter(Calendar.date==datetime(year,month,day)).first().name
+        return Calendar.query.filter(Calendar.date == datetime(year, month, day)).first().name
     else:
-        return Calendar.query.filter(Calendar.date==datetime(datetime.today().year, \
-                                                            datetime.today().month, \
-                                                            datetime.today().day)).first().name
+        return Calendar.query.filter(Calendar.date == datetime(datetime.today().year,
+                                                               datetime.today().month,
+                                                               datetime.today().day)).first().name
+
 
 if __name__ == "__main__":
     app.run(debug=True)

681b72f119fdc5784b5d864490451ef875812df2
Veisakis Manousos <74550377+Veisakis@users.noreply.github.com>
2024-02-23 00:26:39 +0200

    Add files via upload

--- OPC/opc.py
+++ OPC/opc.py
@@ -1,14 +1,38 @@
+import os
+import requests
+import telebot
 from opcua import Client
 
+
 url = "opc.tcp://212.205.81.18:4840/OPCUA/NORDEX-CIF-OPC-UA"
+bot_token = "7046112254:AAHQS_1G4_VXObljR9-kbL_53WIbE54uzXM"
 
-try:
-    client = Client(url)
+node_voltage = "ns=2;s=01CWE50208_analog_ANA000"
+node_power = "ns=2;s=01CWE50208_analog_ANA006"
 
-    client.connect()
+bot = telebot.TeleBot(bot_token)
 
-    tm_voltage = client.get_node("ns=2;s=01CWE50208_analog_ANA000")
-    print(tm_voltage.get_value())
-    client.disconnect()
-except Exception as err:
-    print(err)
+def opc(nodeid):
+    try:
+        client = Client(url)
+        client.connect()
+
+        node = client.get_node(nodeid)
+        value = node.get_value()
+
+        client.disconnect()
+        return value
+    except Exception as err:
+        return err
+
+@bot.message_handler(commands=['v', 'voltage'])
+def voltage(message):
+    value = f'{opc(node_voltage):.2f} kV'
+    bot.reply_to(message, value)
+
+@bot.message_handler(commands=['p', 'power'])
+def power(message):
+    value = f'{opc(node_power):.2f} MW'
+    bot.reply_to(message, value)
+
+bot.infinity_polling()

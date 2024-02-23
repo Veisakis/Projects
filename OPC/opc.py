b52a717ae83ece9ad027e4d8775d59702e3ebb40
Veisakis Manousos <74550377+Veisakis@users.noreply.github.com>
2024-02-23 10:24:45 +0200

    Add files via upload

--- OPC/opc.py
+++ OPC/opc.py
@@ -9,6 +9,9 @@ bot_token = "7046112254:AAHQS_1G4_VXObljR9-kbL_53WIbE54uzXM"
 
 node_voltage = "ns=2;s=01CWE50208_analog_ANA000"
 node_power = "ns=2;s=01CWE50208_analog_ANA006"
+node_system = "ns=2;s=01CWE50208_analog_ANA070"
+node_com = "ns=2;s=01CWE50208_analog_ANA072"
+node_ws = "ns=2;s=01CWE50208_analog_ANA047"
 
 bot = telebot.TeleBot(bot_token)
 
@@ -25,6 +28,11 @@ def opc(nodeid):
     except Exception as err:
         return err
 
+@bot.message_handler(commands=['start', 'help'])
+def voltage(message):
+    value = "Commands\n/v - Voltage at TM\n/p - Power at TM\n/a - Availability\n/w - Wind Speed"
+    bot.reply_to(message, value)
+
 @bot.message_handler(commands=['v', 'voltage'])
 def voltage(message):
     value = f'{opc(node_voltage):.2f} kV'
@@ -33,6 +41,16 @@ def voltage(message):
 @bot.message_handler(commands=['p', 'power'])
 def power(message):
     value = f'{opc(node_power):.2f} MW'
+    bot.reply_to(message, value)
+
+@bot.message_handler(commands=['a', 'availability'])
+def availability(message):
+    value = f'System: {opc(node_system)}/9\nCommunication: {opc(node_com)}/9'
+    bot.reply_to(message, value)
+
+@bot.message_handler(commands=['w', 'wind'])
+def availability(message):
+    value = f'{opc(node_ws):.2f} m/s'
     bot.reply_to(message, value)
 
 bot.infinity_polling()
